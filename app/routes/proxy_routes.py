5d0accf86e710c9e06bbe3d188a1ea9aef734aed
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2025-01-22 15:25:38 -0500

    start proxy routes

---
+++ app/routes/proxy_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+bp = Blueprint("proxy_bp", __name__)
+
+location_key = os.environ.get("RECIPE_KEY")
+
+@bp.get("/recipes")
+def get_recipe():
+    pass
+
+@bp.get("/recipe")
+def get_recipe_by_ingredient():
+    pass
