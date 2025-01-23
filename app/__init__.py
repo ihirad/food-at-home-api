b996e172c6206f234c45d9900d3cc7c8ea97bdb5
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2025-01-23 02:07:24 -0500

    configure dunder init

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS
+import os
+from .db import db, migrate
+from .routes.ingredient_routes import bp as ingredient_bp
+from .routes.shopping_note_routes import bp as shopping_note_bp
+from .models import ingredients, shopping_note
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        app.config.update(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(ingredient_bp)
+    app.register_blueprint(shopping_note_bp)
+
+    return app
