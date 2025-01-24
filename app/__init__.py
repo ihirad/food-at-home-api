b4ca937f15bd872ea6223d98f78c0baebff4f7d8
marjanak <marjana1816@gmail.com>
2025-01-23 22:18:04 -0800

    Done with Migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 import os
 from .db import db, migrate
-from .models import ShoppingNote, UserIngredient, Recipe, Ingredient, User
 from .routes.ingredient_routes import bp as ingredient_bp
 from .routes.shopping_note_routes import bp as shopping_note_bp
 from .routes.proxy_routes import bp as proxy_bp
+from .models import ingredient, shopping_note, user_ingredient, user, recipe
 from .routes.user_routes import bp as user_bp
 from .routes.recipe_routes import bp as recipe_bp
 
