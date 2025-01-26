6145efe420886f90ad28f03a026877cc49083c13
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2025-01-25 21:12:29 -0500

    remove unused file

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from .db import db, migrate
 from .models import shopping_note, user, user_ingredient, recipe, ingredient
 from .routes.ingredient_routes import bp as ingredient_bp
 from .routes.shopping_note_routes import bp as shopping_note_bp
-from .routes.proxy_routes import bp as proxy_bp
 from .routes.user_routes import bp as user_bp
 from .routes.recipe_routes import bp as recipe_bp
 from dotenv import load_dotenv
@@ -31,7 +30,6 @@ def create_app(config=None):
 
     app.register_blueprint(ingredient_bp)
     app.register_blueprint(shopping_note_bp)
-    app.register_blueprint(proxy_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(recipe_bp)
     return app
