a301cba4530fae3df0f491bc4a3d24227f264b76
marjanak <marjana1816@gmail.com>
2025-02-03 13:32:55 -0800

    merged

--- app/__init__.py
+++ app/__init__.py
@@ -40,19 +40,19 @@ def create_app(config=None):
     if config:
         app.config.update(config)
 
-    google = oauth.register(
-        name='google',
-        client_id=app.config["GOOGLE_ID"],
-        client_secret=None,
-        access_token_url='https://accounts.google.com/o/oauth2/token',
-        access_token_params=None,
-        authorize_url='https://accounts.google.com/o/oauth2/auth',
-        authorize_params={"scope": "openid email profile"},
-        api_base_url='https://www.googleapis.com/oauth2/v1/',
-        userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-        client_kwargs={'scope': 'openid email profile'},
-        redirect_uri='http://localhost:5000/google-login'
-    )
+    # google = oauth.register(
+    #     name='google',
+    #     client_id=app.config["GOOGLE_ID"],
+    #     client_secret=None,
+    #     access_token_url='https://accounts.google.com/o/oauth2/token',
+    #     access_token_params=None,
+    #     authorize_url='https://accounts.google.com/o/oauth2/auth',
+    #     authorize_params={"scope": "openid email profile"},
+    #     api_base_url='https://www.googleapis.com/oauth2/v1/',
+    #     userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    #     client_kwargs={'scope': 'openid email profile'},
+    #     redirect_uri='http://localhost:5000/google-login'
+    # )
 
 
     # google = oauth.register(
